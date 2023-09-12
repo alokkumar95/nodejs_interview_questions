cdf31d3b5e6cccbc9e5719c7db0d6e3bba65e064
alok <aloke.kumar95@gmail.com>
2023-09-13 00:09:10 +0530

    add docker

---
+++ DockerDemoAPI/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+
+// end point
+app.get('/',(req,res,next)=>{
+
+    res.status(200).send('Hi')
+    // res.status(200).send(Buffer.from('<p>some html</p>'))
+
+
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is running at ${PORT}`)
+})
